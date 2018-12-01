5cd2d548f64a7443641e6b7d291f5508d37fb486
Santanix <aluno80516@ipt.pt>
2018-12-01 02:37:10 +0000

    seeds, controller, routes, migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+   sen = Sensor.create(idusers: i, sensor_desc: "OH Maluco", max: i*2, min: i/2 )
+   SensorHistory.create(value: i*2, time: Time.now, sensors_historycol: "History of sensor ##{i}", sensors_id:sen.id )
+end
+
