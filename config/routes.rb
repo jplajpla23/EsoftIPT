9022d3a808984bdf0047e9165a3d21f5ff4f4f0f
António <bertil.paiva@gmail.com>
2018-12-01 13:22:04 +0000

    Commit Inical da Autenticação dos Utilizadores

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'authentication_controller/authenticate'
+
+  post 'authenticate', to: 'authentication#authenticate'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
