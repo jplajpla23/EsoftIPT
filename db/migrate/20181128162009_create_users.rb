6a2d68c65e2903606dde2ed9dd7c08ddb33be664
Joao <agostinho.joao@outlook.pt>
2018-12-02 16:48:40 +0000

    migartion autentication update

--- db/migrate/20181128162009_create_users.rb
+++ db/migrate/20181128162009_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
    	  t.integer :role
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.string :remember_token
 
       t.timestamps
