cf19e3dc138779346a4cb71031a60cb4223366f3
Santanix <aluno80516@ipt.pt>
2019-01-21 22:26:17 +0000

    fixed validations on sensor history

--- app/models/sensor_history.rb
+++ app/models/sensor_history.rb
@@ -1,7 +1,8 @@
 class SensorHistory < ApplicationRecord
     #has_one :sensors
     #belongs_to :sensors
-    #belongs_to :sensors,  foreign_key: "sensors_id"
+    belongs_to :sensors,  foreign_key: "sensors_id"
     validates :sensors_id, presence: true
     validates :time, presence: true
+    validates :value, presence: true
 end
