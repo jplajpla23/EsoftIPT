e7ab954349ad32619d6ca50ff450a64daa1f1680
Santanix <aluno80516@ipt.pt>
2019-01-21 21:52:34 +0000

    add test on sensor history

--- test/controllers/sensor_historys_controller_test.rb
+++ test/controllers/sensor_historys_controller_test.rb
@@ -1,6 +1,32 @@
 require 'test_helper'
  class SensorHistorysControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should save sensor" do
+    sensor = sensor_histories(:one)
+    assert sensor.save
+  end
+
+  
+  test "should not save sensor without nothing" do
+    sensor = SensorHistory.new
+    assert_not sensor.save
+  end
+
+  
+  test "should not save sensor without id" do
+    sensor = sensor_histories(:two)
+    assert_not sensor.save
+  end
+
+  
+  test "should not save sensor without value" do
+    sensor = sensor_histories(:three)
+    assert_not sensor.save
+  end
+
+  test "should not save sensor without time" do
+    sensor = sensor_histories(:four)
+    assert_not sensor.save
+  end
+
 end
