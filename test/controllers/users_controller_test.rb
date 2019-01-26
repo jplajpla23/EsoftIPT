089a65e346c6d6a2a6abb240cec9f68debce6def
Joao <agostinho.joao@outlook.pt>
2019-01-26 16:31:28 +0000

    no message

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,10 +6,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
   end
 
- # test "should not save user without nothing" do
-  #  user = User.new
-  #  assert_raise user.save
- # end
+  test "should not save user without nothing" do
+    user = User.new
+    user.password="teste"
+    assert_raise user.save
+  end
 
   test "should save user" do
     user = users(:one)
