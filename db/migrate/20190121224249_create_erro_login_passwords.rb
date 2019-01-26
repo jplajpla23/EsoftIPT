4f300ae51c9cd6ed466b9d00c64e1a612571c406
António <bertil.paiva@gmail.com>
2019-01-26 02:28:23 +0000

    Correção tabela erros

--- db/migrate/20190121224249_create_erro_login_passwords.rb
+++ db/migrate/20190121224249_create_erro_login_passwords.rb
@@ -1,7 +1,7 @@
 class CreateErroLoginPasswords < ActiveRecord::Migration[5.2]
   def change
     create_table :erro_login_passwords do |t|
-      t.integer :id
+      t.integer id:false
       t.string :pagina
       t.string :mensagem
       t.timestamps
