a74cea6580f0430e7c767b6547ebbd84d39aebaa
joaogracio <estt16172@ipt.pt>
2019-01-28 20:22:38 +0000

    Merge branch 'gracio' of https://github.com/jplajpla23/EsoftIPT
    Teste inicial unitario para Alert
    
    # Conflicts:
    #	app/models/alert.rb
    #	config/routes.rb
    #	test/fixtures/alerts.yml
    #	test/models/alert_test.rb

--- test/models/alert_test.rb
+++ test/models/alert_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class AlertTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save alert with out a sensor, min, max, message" do 
+    alert = Alert.new
+    alert.sensors_id = Sensor.all.first.id
+    assert_not alert.save, "Saved the alert with out a senor, min, max, message"
+  end
 end
